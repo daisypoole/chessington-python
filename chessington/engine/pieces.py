5ae4617367f3162ecc5197d19e67a3ab83f5b353
daisypoole <71255075+daisypoole@users.noreply.github.com>
2020-09-23 13:35:38 +0100

    Sideways Rook

--- chessington/engine/pieces.py
+++ chessington/engine/pieces.py
@@ -117,16 +117,15 @@ class Rook(Piece):
         currentP = board.find_piece(self)
 
         if white:
-            squareone = Square.at(currentP.row + 1, currentP.col)
-            if board.get_piece(squareone) is None:
-                castle.append(Square.at(currentP.row + 6, currentP.col))
+            #squareone = Square.at(currentP.row + 1 and currentP.row+6, currentP.col)
+            #if board.get_piece(squareone) is None:
 
-
-            #castle.append(Square.at(currentP.row+2, currentP.col))
-            #castle.append(Square.at(currentP.row+3, currentP.col))
-            #castle.append(Square.at(currentP.row+4, currentP.col))
-            #castle.append(Square.at(currentP.row+5, currentP.col))
-            #castle.append(Square.at(currentP.row+6, currentP.col))
+            castle.append(Square.at(currentP.row+1 and currentP.row, currentP.col+1))
+            castle.append(Square.at(currentP.row+2, currentP.row))
+            castle.append(Square.at(currentP.row+3, currentP.row))
+            castle.append(Square.at(currentP.row+4, currentP.col))
+            castle.append(Square.at(currentP.row+5, currentP.col))
+            castle.append(Square.at(currentP.row+6, currentP.col))
 
 
         else:
